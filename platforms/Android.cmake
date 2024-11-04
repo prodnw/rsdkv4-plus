627d2f69c2433cb08a659fb339cdc7f38dfa7561
Jdsle <Jd1.ext4@gmail.com>
2024-11-04 12:37:22 -0500

    Fix platform cmake files to build theora

--- platforms/Android.cmake
+++ platforms/Android.cmake
@@ -6,9 +6,11 @@ set(RETRO_USE_HW_RENDER ON)
 set(RETRO_OUTPUT_NAME "RetroEngine")
 
 set(COMPILE_OGG TRUE)
+set(COMPILE_THEORA TRUE)
 set(COMPILE_VORBIS TRUE)
 
 set(OGG_FLAGS -ffast-math -fsigned-char -O2 -fPIC -DPIC -DBYTE_ORDER=LITTLE_ENDIAN -D_ARM_ASSEM_ -w)
+set(THEORA_FLAGS -ffast-math -fsigned-char -O2 -fPIC -DPIC -DBYTE_ORDER=LITTLE_ENDIAN -D_ARM_ASSEM_ -w)
 set(VORBIS_FLAGS -ffast-math -fsigned-char -O2 -fPIC -DPIC -DBYTE_ORDER=LITTLE_ENDIAN -D_ARM_ASSEM_ -w)
 
 target_link_libraries(RetroEngine
