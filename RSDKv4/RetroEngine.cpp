3834d28d31d5e6e86fc7a7249c1b81a4f37ec9fa
ProdNW <79802429+prodnw@users.noreply.github.com>
2025-05-21 16:19:21 +0100

    put Discord RPC behind a global variable check

--- RSDKv4/RetroEngine.cpp
+++ RSDKv4/RetroEngine.cpp
@@ -575,8 +575,10 @@ void RetroEngine::Init()
 #endif
 
 #if RETRO_USE_DISCORD_SDK
-    API_Init();
-    API_Discord_SetRPCStatus("Test1", "Test2", "Test3", "Test4", "Test5", "Test6");
+    if (GetGlobalVariableByName("engine.useDiscordRPC") == true) {
+        API_Init();
+        API_Discord_SetRPCStatus("Test1", "Test2", "Test3", "Test4", "Test5", "Test6");
+    }
 #endif
 }
 
