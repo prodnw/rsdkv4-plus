174f91f072ca7e22130a35b12334ec9485fe4be4
EggBanana123 <138070802+EggBanana123@users.noreply.github.com>
2025-12-06 16:00:36 +0000

    nvm this was necessary

--- RSDKv4/Scene.cpp
+++ RSDKv4/Scene.cpp
@@ -350,6 +350,7 @@ void ProcessStage(void)
             gfxVertexSizeOpaque = 0;
 #endif
 
+            FlipFrameBuffer(screenDirection);
             DrawObjectList(0);
             DrawObjectList(1);
             DrawObjectList(2);
@@ -526,6 +527,7 @@ void ProcessStage(void)
 
                 keyPress[0].C = false;
                 ProcessPausedObjects();
+                FlipFrameBuffer(screenDirection);
                 DrawObjectList(0);
                 DrawObjectList(1);
                 DrawObjectList(2);
