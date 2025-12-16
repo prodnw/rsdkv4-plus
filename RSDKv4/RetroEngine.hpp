e0d44d8698e2cd309c0094184e826d5d960a36be
ProdNW <79802429+prodnw@users.noreply.github.com>
2025-12-16 16:00:41 +0000

    fixes

--- RSDKv4/RetroEngine.hpp
+++ RSDKv4/RetroEngine.hpp
@@ -127,22 +127,14 @@ typedef unsigned int uint;
 #define RETRO_USING_SDL2 (0)
 #endif
 
-// NOTE: This is only used for rev00 stuff, it was removed in rev01 and later builds
-#if RETRO_PLATFORM <= RETRO_WP7
-#define RETRO_GAMEPLATFORMID (RETRO_PLATFORM)
-#else
-
-// use *this* macro to determine what platform the game thinks its running on (since only the first 7 platforms are supported natively by scripts)
-#if RETRO_PLATFORM == RETRO_LINUX
-#define RETRO_GAMEPLATFORMID (RETRO_WIN)
-#elif RETRO_PLATFORM == RETRO_UWP
-#define RETRO_GAMEPLATFORMID (UAP_GetRetroGamePlatformId())
+#if RETRO_PLATFORM == RETRO_iOS || RETRO_PLATFORM == RETRO_ANDROID || RETRO_PLATFORM == RETRO_WP7
+#define RETRO_GAMEPLATFORM (RETRO_MOBILE)
 #elif RETRO_PLATFORM == RETRO_SWITCH
 #define RETRO_GAMEPLATFORMID (RETRO_MOBILE)
+#elif RETRO_PLATFORM == RETRO_UWP
+#define RETRO_GAMEPLATFORM (UAP_GetRetroGamePlatform())
 #else
-#error Unspecified RETRO_GAMEPLATFORMID
-#endif
-
+#define RETRO_GAMEPLATFORM (RETRO_STANDARD)
 #endif
 
 #define RETRO_SW_RENDER (0)
@@ -156,14 +148,12 @@ typedef unsigned int uint;
 #define RETRO_RENDERTYPE (RETRO_SW_RENDER)
 #endif
 
-#if RETRO_PLATFORM == RETRO_ANDROID || RETRO_PLATFORM == RETRO_SWITCH
+#ifndef RETRO_USING_OPENGL
 #define RETRO_USING_OPENGL (1)
-#else
-#define RETRO_USING_OPENGL (0)
 #endif
 
 #define RETRO_SOFTWARE_RENDER (RETRO_RENDERTYPE == RETRO_SW_RENDER)
-#define RETRO_HARDWARE_RENDER (RETRO_RENDERTYPE == RETRO_HW_RENDER)
+//#define RETRO_HARDWARE_RENDER (RETRO_RENDERTYPE == RETRO_HW_RENDER)
 
 #if RETRO_USING_OPENGL
 #if RETRO_PLATFORM == RETRO_ANDROID
@@ -213,13 +203,9 @@ typedef unsigned int uint;
 #define GL_FRAMEBUFFER         GL_FRAMEBUFFER_EXT
 #define GL_COLOR_ATTACHMENT0   GL_COLOR_ATTACHMENT0_EXT
 #define GL_FRAMEBUFFER_BINDING GL_FRAMEBUFFER_BINDING_EXT
-#elif RETRO_PLATFORM == RETRO_SWITCH
-#include <GLES/gl.h>
-#include <EGL/egl.h>
-#include <EGL/eglext.h>
-#include <glad/glad.h>  // OpenGL loader
 #else
 #include <GL/glew.h>
+#endif
 #endif
 
 #define RETRO_USE_HAPTICS (1)
